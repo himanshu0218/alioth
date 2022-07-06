77253bdf12f8cf4c62409f281fa66fa38523cfa7
Himanshu <bhaduriahimanshu@gmail.com>
2022-07-06 04:44:33 +0000

    alioth : superior-ify
    Signed-off-by: Himanshu <bhaduriahimanshu@gmail.com>

--- superior_alioth.mk
+++ superior_alioth.mk
@@ -8,26 +8,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/superior/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := superior_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 
-# LineagePlus flags
-BUILD_TYPE := OFFICIAL
-TARGET_INCLUDE_GAPPS := true
-TARGET_INCLUDE_LAWNCHAIR = true
+# Some flags
+BUILD_WITH_PIXEL_GAPPS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
-PLUS_MAINTAINER := cristianvaz
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
